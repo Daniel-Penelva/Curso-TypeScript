1166f37540afa65f63aba8b1170f7b26290b2420
Daniel Penelva <d4n.andrade@gmail.com>
2023-06-30 20:11:07 -0300

    Funções

--- main.ts
+++ main.ts
@@ -1,10 +1,44 @@
 
-// Objetos
-let nome: object
-nome ={nome: 'Daniel', sobrenome: 'Penelva', idade: 34, altura: 1.75 }
+// Funções
 
-let objeto: Record<string, string>
-objeto ={nome: 'Daniel', sobrenome: 'Penelva', idade: '34', altura: '1.75' }
+function oi(): void{
+    console.log('oi')
+}
 
-let objeto1: {nome: string, idade: number, ativo: true}
-objeto1 ={nome: 'Daniel', idade: 34, ativo: true}
+function nome(): string{
+    return 'Daniel'
+}
+
+function soma(): number{
+    
+    let soma, num1, num2: number
+    num1 = 10, num2 = 20
+
+    soma = num1 + num2
+    return soma
+}
+
+function nomeCompleto(nome: string, sobrenome:string):string{
+    nome = 'Daniel'
+    sobrenome = 'Penelva'
+    return 'Seu nome completo é:' + nome + ' ' + sobrenome
+}
+
+// O TypeScript consegue deduzir que a função é do tipo number, já que seus parâmetros recebem um tipo number
+function subtracao(num1:number, num2:number){
+    num1 = 10
+    num2 = 5
+
+    let subtracao = num1 - num2
+    return subtracao
+}
+
+// Função com seta (arrow function)
+const multiplicacao = (num1:number, num2:number): number => num1 + num2
+
+const divisao = (num1:number, num2:number) =>{
+    
+    num1 = 10, num2 = 5
+    let divisao = num1/num2
+    num1 + num2
+} 
